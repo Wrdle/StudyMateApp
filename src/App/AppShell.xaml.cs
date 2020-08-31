b35a8f03927db1898ba293e68014b0be499b38eb
Maki <maki@vexit.com>
2020-08-31 10:15:26 +1000

    Initial commit.

---
+++ src/App/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using App.ViewModels;
+using App.Views;
+using Xamarin.Forms;
+
+namespace App
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
