83d8107de0cdec67f6110ec068a795be11872197
Maki <maki@vexit.com>
2020-09-07 10:36:26 +1000

    Fixed: removed reference to non existent class

--- src/Mobile/Mobile/ViewModels/BaseViewModel.cs
+++ src/Mobile/Mobile/ViewModels/BaseViewModel.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
-
-using Xamarin.Forms;
-
-using Mobile.Models;
-using Mobile.Services;
-
-namespace Mobile.ViewModels
+﻿namespace Mobile.ViewModels
 {
     public class BaseViewModel : MvvmHelpers.BaseViewModel
     {
-        public IDataStore<Item> DataStore => DependencyService.Get<IDataStore<Item>>();
     }
 }
