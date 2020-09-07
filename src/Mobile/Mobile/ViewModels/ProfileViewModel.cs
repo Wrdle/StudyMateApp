df2f10aff642a0f6216d7d28329f497ab4f58f52
Matthew D'Agostino <mdagostino175@gmail.com>
2020-09-07 10:24:33 +1000

    More Navigation Updates
    - Changed to using scroll view with stacklayouts for all pages
    - Added more view models
    - Removed templates (not implementing this feature for MVP)

---
+++ src/Mobile/Mobile/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mobile.ViewModels
+{
+    class ProfileViewModel : BaseViewModel
+    {
+        public ProfileViewModel()
+        {
+            Title = "Profile";
+        }
+    }
+}
