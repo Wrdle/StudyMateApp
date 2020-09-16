cf6c8d43358002846daa10d3094c5a31d6c073f0
Matthew D'Agostino <mdagostino175@gmail.com>
2020-09-16 16:33:58 +1000

    Created SMColour and SMColours classes
    - Assist in storing colours for an assignment
    - Stores both background and font colours
    - Predefined list of colours with compatible text colours

---
+++ src/Mobile/Mobile/Helpers/SMColour.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Mobile.Helpers
+{
+    public class SMColour
+    {
+        public Color BackgroundColour;
+        public Color FontColour;
+
+        public SMColour(Color backgroundColour, Color fontColour)
+        {
+            BackgroundColour = backgroundColour;
+            FontColour = fontColour;
+        }
+
+        public SMColour(string hexBackgroundColour, string hexFontColour)
+        {
+            BackgroundColour = Color.FromHex(hexBackgroundColour);
+            FontColour = Color.FromHex(hexFontColour);
+        }
+
+        public SMColour(string hexBackgroundColour, Color fontColour)
+        {
+            BackgroundColour = Color.FromHex(hexBackgroundColour);
+            FontColour = fontColour;
+        }
+    }
+}
