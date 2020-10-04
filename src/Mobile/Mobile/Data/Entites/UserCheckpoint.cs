2bf24d6822b2d23d78daa59ede8fa6e58a3d2dca
Maki <maki@vexit.com>
2020-10-05 09:59:50 +1000

    Added checkpoint entity to databse for assignments. Updated assignment store to also return checkpoints.

---
+++ src/Mobile/Mobile/Data/Entites/UserCheckpoint.cs
@@ -0,0 +1,13 @@
+﻿namespace Mobile.Data.Entites
+{
+    public class UserCheckpoint
+    {
+        public long UserId { get; set; }
+        public long CheckpointId { get; set; }
+
+        // Referenced Entities
+        public User User { get; set; }
+        public Checkpoint Checkpoint { get; set; }
+
+    }
+}
