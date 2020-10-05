4a90d88a0789c32e4ac691a5255ee6f5e6d59c56
Matthew D'Agostino <mdagostino175@gmail.com>
2020-10-05 21:54:59 +1000

    Create Helpers.cs
    - Added function to automatically turn a given list into an observable collection

---
+++ src/Mobile/Mobile/Helpers/Helpers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Mobile.Helpers
+{
+    public class Helpers
+    {
+        public static ObservableCollection<T> convertListToObservableCollection<T>(List<T> list)
+        {
+            var oc = new ObservableCollection<T>();
+            foreach (var item in list)
+                oc.Add(item);
+            return oc;
+        }
+    }
+}
