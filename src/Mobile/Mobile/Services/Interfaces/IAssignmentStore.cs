ac3f399e68c3da9a5a514e2e4376a573259dd1f7
Matthew D'Agostino <mdagostino175@gmail.com>
2020-10-05 21:56:22 +1000

    Added new assignment store function

--- src/Mobile/Mobile/Services/Interfaces/IAssignmentStore.cs
+++ src/Mobile/Mobile/Services/Interfaces/IAssignmentStore.cs
@@ -7,10 +7,12 @@ namespace Mobile.Services.Interfaces
     public interface IAssignmentStore<T>
     {
         // Commands
+        Task<bool> AddAssignmentAsync(T assignment);
 
         // Queries
         //Task<ICollection<AssignmentListItem>> GetByGroup(long groupId);
         Task<Assignment> GetById(long id);
         Task<ICollection<T>> GetAllByUserAsync(int userID, bool forceRefresh = false);
+        Task<long> GenerateNewAssignmentID();
     }
 }
