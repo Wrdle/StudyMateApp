1e58bcabe9685515e24bb65dbb46a3dd124eaacd
Maki <maki@vexit.com>
2020-10-06 23:09:19 +1000

    Added 'Add Group' view model.

---
+++ src/Mobile/Mobile/ViewModels/Groups/AddGroupViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mobile.ViewModels.Groups
+{
+    public class AddGroupViewModel : BaseViewModel
+    {
+        //------------------------------
+        //          Fields
+        //------------------------------
+
+
+        //------------------------------
+        //          Constructors
+        //------------------------------
+
+        public AddGroupViewModel()
+        {
+
+        }
+
+        //------------------------------
+        //          Methods
+        //------------------------------
+
+
+    }
+}
