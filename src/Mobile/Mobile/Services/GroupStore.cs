087e56243d121cd49b30e1c3e77f97caa892cb40
Maki <maki@vexit.com>
2020-10-21 17:28:00 +1000

    Pass assignments to group model when getting group by Id.

--- src/Mobile/Mobile/Services/GroupStore.cs
+++ src/Mobile/Mobile/Services/GroupStore.cs
@@ -21,6 +21,7 @@ namespace Mobile.Services
         //------------------------------
 
         private readonly IUserStore _userStore;
+        private readonly IAssignmentStore _assignmentStore;
         private readonly ImageConverter _imageConverter;
 
         //------------------------------
@@ -30,6 +31,7 @@ namespace Mobile.Services
         public GroupStore()
         {
             _userStore = DependencyService.Get<IUserStore>();
+            _assignmentStore = DependencyService.Get<IAssignmentStore>();
             _imageConverter = DependencyService.Get<ImageConverter>();
         }
 
@@ -132,12 +134,16 @@ namespace Mobile.Services
                 var group = await dbContext.Groups
                     .Include(g => g.CoverColor)
                     .SingleOrDefaultAsync(g => g.Id == id);
+
+                var assignments = await _assignmentStore.GetByGroupId(id);
+
                 return new Group
                 {
                     Id = group.Id,
                     Name = group.Name,
                     CoverPhoto = _imageConverter.BytesToImage(group.CoverPhoto),
-                    CoverColor = new CoverColor { Id = group.CoverColor.Id, BackgroundColor = group.CoverColor.BackgroundColorFromHex, FontColor = group.CoverColor.FontColorFromHex }
+                    CoverColor = new CoverColor { Id = group.CoverColor.Id, BackgroundColor = group.CoverColor.BackgroundColorFromHex, FontColor = group.CoverColor.FontColorFromHex },
+                    Assignments = assignments
                 };
             }
         }
